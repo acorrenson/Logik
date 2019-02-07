7107fd51f2ef5acdd6141272d97fa05f2e883ca3
Arthur Correnson <arthur.correnson@gmail.com>
2019-02-07 19:34:47 +0100

    Add ENV to build Truth table

--- evaluator.py
+++ evaluator.py
@@ -1,21 +1,63 @@
 from parser import *
+from itertools import product
 
-def evaluate(ast):
+
+def get_var(ast):
+    var = []
+    def r_get_var(ast):
+        op = ast[0]
+        if op == 'symb':
+            if ast[1] not in var:
+                var.append(ast[1])
+        elif op == 'non':
+            r_get_var(ast[1])
+        else:
+            r_get_var(ast[1])
+            r_get_var(ast[2])
+
+    r_get_var(ast)
+    return var
+
+
+
+
+def make_env(var):
+    tab = list(product([0, 1], repeat=len(var)))
+    env = []
+    for lines in tab:
+        r = {}
+        for i, v in enumerate(var):
+            r[v] = lines[i]
+        env.append(r)
+    return (env, tab)
+
+
+def evaluate(ast, env):
     op = ast[0]
 
     if op == 'symb':
-        # TODO : evaluate according to ENV
-        return 1
+        return env[ast[1]]
     elif op == 'value':
         return ast[1]
-    elif op == 'unop':
-        return 1 - evaluate(ast[1])
+    elif op == 'non':
+        return 1 - evaluate(ast[1], env)
     elif op == 'ou':
-        return max(evaluate(ast[1]), evaluate(ast[2]))
+        return max(evaluate(ast[1], env), evaluate(ast[2], env))
     elif op == 'et':
-        return min(evaluate(ast[1]), evaluate(ast[2]))
+        return min(evaluate(ast[1], env), evaluate(ast[2], env))
     elif op == '->':
-        return max(1 - evaluate(ast[1]), evaluate(ast[2]))
+        return max(1 - evaluate(ast[1], env), evaluate(ast[2], env))
+
+
+def evaluate_all(ast):
+    var = get_var(ast)
+    env, tab = make_env(var)
+    
+    print(*var)
+    print('--'*(len(var)))
+    for i, row in enumerate(env):
+        print(*tab[i], end=' ')
+        print(evaluate(ast, row))
 
 
 if __name__ == '__main__':
@@ -28,7 +70,7 @@ if __name__ == '__main__':
     ast = parse(seq)
     # display the AST
     pprint(ast)
-    print("\nRESULT : ", end=" ")
-    print(evaluate(ast))
+    print("\nTruth table : \n")
+    evaluate_all(ast)
 
         
