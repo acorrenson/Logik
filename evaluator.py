4cc6dc4be54d2edd3e9bc9ab1c40939796132194
Arthur Correnson <arthur.correnson@gmail.com>
2019-05-13 18:35:55 +0200

    Improve doc

--- evaluator.py
+++ evaluator.py
@@ -2,63 +2,73 @@ from parser import *
 from itertools import product
 
 
-def get_var(ast):
-    var = []
+def get_vars(ast):
+    """List all free variables in an expression"""
+    var_list = []
     def r_get_var(ast):
-        op = ast[0]
-        if op == 'value':
+        typ = ast[0]
+        if typ == 'value':
             return
-        elif op == 'symb':
-            if ast[1] not in var:
-                var.append(ast[1])
-        elif op == 'non':
+        elif typ == 'symb':
+            if ast[1] not in var_list:
+                var_list.append(ast[1])
+        elif typ == 'non':
             r_get_var(ast[1])
         else:
             r_get_var(ast[1])
             r_get_var(ast[2])
 
     r_get_var(ast)
-    return var
+    return var_list
 
 
-def make_env(var):
-    tab = list(product([0, 1], repeat=len(var)))
-    env = []
+def make_env(var_list):
+    """Build a truth table for an expression"""
+    tab = list(product([0, 1], repeat=len(var_list)))
+    env_list = []
     for lines in tab:
-        r = {}
-        for i, v in enumerate(var):
-            r[v] = lines[i]
-        env.append(r)
-    return (env, tab)
+        env = {}
+        for i, v in enumerate(var_list):
+            env[v] = lines[i]
+        env_list.append(env)
+    return (env_list, tab)
 
 
 def evaluate(ast, env):
-    op = ast[0]
+    """
+        Evaluate an expression according to the valuation 
+        described by 'env'
+    """
+    typ = ast[0]
 
-    if op == 'symb':
+    if typ == 'symb':
         return env[ast[1]]
-    elif op == 'value':
+    elif typ == 'value':
         return ast[1]
-    elif op == 'non':
+    elif typ == 'non':
         return 1 - evaluate(ast[1], env)
-    elif op == 'ou':
+    elif typ == 'ou':
         return max(evaluate(ast[1], env), evaluate(ast[2], env))
-    elif op == 'et':
+    elif typ == 'et':
         return min(evaluate(ast[1], env), evaluate(ast[2], env))
-    elif op == '->':
+    elif typ == '->':
         return max(1 - evaluate(ast[1], env), evaluate(ast[2], env))
 
 
 def evaluate_all(ast):
-    var = get_var(ast)
-    env, tab = make_env(var)
+    """
+        Evaluate an expression and if it contains free variables, 
+        display the truth table.
+    """
+    var_list = get_vars(ast)
+    envs, tab = make_env(var_list)
     
-    if len(var) > 0:
+    if len(var_list) > 0:
         print("\nTruth table : \n")
-        print(*var)
-        print('--'*(len(var)))
+        print(*var_list)
+        print('--'*(len(var_list)))
         
-        for i, row in enumerate(env):
+        for i, row in enumerate(envs):
             print(*tab[i], end=' ')
             print(evaluate(ast, row))
     else:
